6311441b285c8c02199ee30edc9a8d99dc73fc80
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2026-06-30 17:27:32 -0400

    changes

--- admart_technology/settings.py
+++ admart_technology/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'mysite',
     'account',
-    'admin',
     'guest',
     'instructor',
     'student',
--- admart_technology/urls.py
+++ admart_technology/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin, include
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mysite.urls')),
     path('account/', include('account.urls')),
-    path('administrator/', include('admin.urls')),
     path('guest/', include('guest.urls')),
     path('instructor/', include('instructor.urls')),
     path('student/', include('student.urls')),
